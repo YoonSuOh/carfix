06351a90bfb76107df2e85d2eb899de7f4baf322
suohyoon <dbstndh12@gmail.com>
2023-11-21 05:50:17 +0900

    환경 설정

---
+++ src/main/java/com/carfix/CarfixApplication.java
@@ -0,0 +1,13 @@
+package com.carfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carfix/CarfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
