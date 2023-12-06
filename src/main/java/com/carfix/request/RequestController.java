b2a2402c7aa9d78e1d473618ae747b7365699bbe
SuOhYoon <dbstndh12@gmail.com>
2023-12-06 16:59:51 +0900

    .

--- src/main/java/com/carfix/request/RequestController.java
+++ src/main/java/com/carfix/request/RequestController.java
@@ -16,6 +16,8 @@ import java.util.Map;
 @RequestMapping("/requests/")
 public class RequestController {
     private final Map<String, Object> userInfo = new HashMap<>();
+    private final Map<String, Object> carInfo = new HashMap<>();
+    private final Map<String, Object> fixInfo = new HashMap<>();
 
     // 수리 요청 화면 띄우기
     @GetMapping("/create")
@@ -23,21 +25,44 @@ public class RequestController {
         return "request/create";
     }
 
-    @GetMapping("/create/1")
+    // 차량정보 입력 창으로 넘기기
+    @GetMapping("/create/step0")
     public String completeStep1(
             @RequestParam("name") String name,
             @RequestParam("phoneNumber") String phoneNumber,
             @RequestParam("email") String email){
-          log.info(name);
           userInfo.put("name", name);
           userInfo.put("phoneNumber", phoneNumber);
           userInfo.put("email", email);
-        return "forward:/requests/create/2";
+        return "forward:/requests/create/step1";
     }
 
-    @GetMapping("/create/2")
+
+    // 차량정보 입력 창 띄우기
+    @GetMapping("/create/step1")
     public String getStep2(){
-        log.info((String) userInfo.get("name"));
         return "request/create2";
+    }
+
+
+    // 파손부위 등록 창으로 넘기기
+    @GetMapping("/create/step2")
+    public String completeStep2(
+            @RequestParam("model") String model,
+            @RequestParam("carnum") String carnum,
+            @RequestParam("year") int year,
+            @RequestParam("fuel") String fuel){
+        carInfo.put("model", model);
+        carInfo.put("carnum", carnum);
+        carInfo.put("year", year);
+        carInfo.put("fuel", fuel);
+        return "forward:/requests/create/step3";
+    }
+
+
+    // 파손부위 등록 창 띄우기
+    @GetMapping("/create/step3")
+    public String getStep3(){
+        return "request/create3";
     }
 }
